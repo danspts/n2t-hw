6e210af22fddb0b3bef7378ff2694b113f9448ff
Doron Leibovici <doron3199@gmail.com>
2021-05-11 11:10:38 +0300

    doron 7 init

--- 06/assembler/HackAssembler.py
+++ 06/assembler/HackAssembler.py
@@ -10,7 +10,8 @@ def main(file_name):
     compiled_code = assembler.compile()
     compiled_code = assembler.address_init(compiled_code)
     compiled_code = assembler.parse_symbols(compiled_code)
-    print(compiled_code[:])
+    print(compiled_code)
+    compiled_code = assembler.to_binary(compiled_code)
     new_name = assembler.file_name.replace(".asm", ".hack")
     with open(new_name, "w") as writer:
         writer.write(compiled_code)
@@ -47,8 +48,8 @@ class HackAssembler:
         for line in compiled_code.split('\n'):
             if '@' in line:
                 if line[1:].isnumeric():
-                    if not self.symbol_table.contains(line[1:]):
-                        self.symbol_table.add_entry(line[1:], int(line[1:]))
+                    # if not self.symbol_table.contains(line[1:]):
+                    #     self.symbol_table.add_entry(line[1:], int(line[1:]))
                     symbol_less += line + '\n'
                 else:
                     if self.symbol_table.contains(line[1:]):
@@ -61,6 +62,32 @@ class HackAssembler:
                 symbol_less += line + '\n'
         return symbol_less
 
+    def to_binary(self, compiled_code):
+        binary = ''
+        for line in compiled_code.strip().split('\n'):
+            if line[0] == '@':
+                bin_num = str(bin(int(line[1:])))[2:]
+                binary += (16 - len(bin_num)) * '0' + bin_num
+            else:
+                dest = 'NULL'
+                jump = 'NULL'
+                comp = line.split('=')
+                if len(comp) == 2:
+                    dest = comp[0]
+                    comp = comp[1]
+                else:
+                    comp = comp[0]
+                comp = comp.split(';')
+                if len(comp) == 2:
+                    jump = comp[1]
+                comp = comp[0]
+                binary += '111' + self.code.comp_dict[comp] + self.code.dest_dict[dest] + self.code.jump_dict[jump]
+            if binary[-16:] == '0000000000010001':
+                print('hi')
+
+            binary += '\n'
+
+        return binary
 
 
 if __name__ == '__main__':
---
+++ 07/VMTranslator/VMTranslator.py
@@ -0,0 +1,9 @@
+import sys
+
+def main(file_name):
+    pass
+
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    main(file_name)
